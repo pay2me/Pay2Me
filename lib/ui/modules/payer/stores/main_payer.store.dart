75bc207d28d1bb2a409c3ff89e2dd8bedbd828d7
oscarbfs <oscar.borgesfs@gmail.com>
2023-08-18 18:49:21 -0300

    tirando mock

--- lib/ui/modules/payer/stores/main_payer.store.dart
+++ lib/ui/modules/payer/stores/main_payer.store.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:pay_2_me/domain/models/export_models.dart';
-import 'package:pay_2_me/domain/models/mapper/set_service_mapper.dart';
 import 'package:pay_2_me/ui/modules/search/export_search.dart';
 import 'package:provider/provider.dart';
 import 'package:pay_2_me/ui/modules/export_modules.dart';
@@ -15,64 +14,7 @@ abstract class _MainPayerStore with Store, ChangeNotifier {
   @observable
   bool isLoading = false;
 
-  List<SetPayerMapper> _payerList = [
-    SetPayerMapper(
-      payerId: "1",
-      payerName: "Oscar Borges Faleta Santana",
-      payerCpf: "123.456.789-10",
-      payerPhone: "+55 (71) 997335815",
-      payerCnpj: "123.456.78/1234-56",
-      payerCompanyName: "Pay2Me",
-      payerAddress: SetAddressMapper(
-        addressCEP: "12345-678",
-        addressCity: "Algoinhas",
-        addressNeighborhood: "Centro",
-        addressState: "Bahia",
-      ),
-      payerCard: SetCardMapper(
-        cardId: "1",
-        cardCvv: "123",
-        cardExpiryDate: DateTime.now(),
-        cardNumber: "1234.5678.9012.3456",
-        cardPrintedName: "Oscar B F Santana",
-      ),
-      payerService: SetServiceMapper(
-        serviceName: "Qualquer Serviço",
-        serviceValue: "200",
-        serviceFrequency: "12 meses",
-        serviceExpirationPlanDate: DateTime.now().add(Duration(days: 30)),
-        serviceSubscriptionExpirationDate: DateTime.now().add(Duration(days: 90)),
-      )
-    ),
-    SetPayerMapper(
-      payerId: "2",
-      payerName: "Elton Faleta",
-      payerCpf: "123.456.789-10",
-      payerPhone: "+55 (71) 997335815",
-      payerCnpj: "123.456.78/1234-56",
-      payerCompanyName: "Pay2Me",
-      payerAddress: SetAddressMapper(
-        addressCEP: "12345-678",
-        addressCity: "Algoinhas",
-        addressNeighborhood: "Centro",
-        addressState: "Bahia",
-      ),
-      payerCard: SetCardMapper(
-        cardId: "2",
-        cardCvv: "123",
-        cardExpiryDate: DateTime.now(),
-        cardNumber: "1234.5678.9012.3456",
-        cardPrintedName: "Elton F Santana",
-      ),
-      payerService: SetServiceMapper(
-        serviceName: "Outro Serviço",
-        serviceValue: "200",
-        serviceFrequency: "12 meses",
-        serviceExpirationPlanDate: DateTime.now().subtract(Duration(days: 90)),
-        serviceSubscriptionExpirationDate: DateTime.now().subtract(Duration(days: 30)),
-      )
-    )
-  ];
+  List<SetPayerMapper> _payerList = [];
 
   @observable
   List<SetPayerMapper> payersToOverview = [];
